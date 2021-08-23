147f3bb4fada8ca4fdc06f6628852d08309ed7a6
Meleg Zoltán <zoltan.meleg@generali.com>
2021-08-23 14:40:42 +0200

    implemented mark and unmark methods

--- src/Codecool.EinsenhowerMatrix/TodoItem.cs
+++ src/Codecool.EinsenhowerMatrix/TodoItem.cs
@@ -53,6 +53,7 @@ namespace Codecool.EinsenhowerMatrix
         /// </summary>
         public void Mark()
         {
+            IsDone = true;
         }
 
         /// <summary>
@@ -60,6 +61,7 @@ namespace Codecool.EinsenhowerMatrix
         /// </summary>
         public void UnMark()
         {
+            IsDone = false;
         }
 
         /// <summary>
