aab99d746dc472faeefe6c5f4e589803455fcce0
KrigerGabor <gabor.kriger@gmail.com>
2021-08-24 14:11:17 +0200

    Main menu

--- src/Codecool.EinsenhowerMatrix/EisenhowerMain.cs
+++ src/Codecool.EinsenhowerMatrix/EisenhowerMain.cs
@@ -7,11 +7,37 @@ namespace Codecool.EinsenhowerMatrix
     /// </summary>
     public class EisenhowerMain
     {
+        private bool _cmd = true;
+
         /// <summary>
         /// Runs program with basic user UI
         /// </summary>
         public void Run()
         {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[ 1 ] Display all quarter");
+                Console.WriteLine("[ 2 ] Display specific quarter");
+                Console.WriteLine("[ 3 ] Create task");
+                Console.WriteLine("[ 4 ] Delete task");
+                Console.WriteLine("[ 5 ] Archive all tasks");
+                Console.WriteLine();
+                Console.WriteLine("[ 0 ] Exit program");
+
+                int option;
+                bool inputIsValid = int.TryParse(Console.ReadLine(), out option);
+                if (inputIsValid)
+                {
+                    switch (option)
+                    {
+                        case 0:
+                            _cmd = false;
+                            break;
+                    }
+                }
+            }
+            while (_cmd);
         }
     }
 }
