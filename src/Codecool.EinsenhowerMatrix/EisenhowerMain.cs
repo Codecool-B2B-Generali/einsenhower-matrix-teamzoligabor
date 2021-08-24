c929b97f36279980c2eb7f2ea71f31062daff0d6
KrigerGabor <gabor.kriger@gmail.com>
2021-08-24 14:45:17 +0200

    QuarterMenu

--- src/Codecool.EinsenhowerMatrix/EisenhowerMain.cs
+++ src/Codecool.EinsenhowerMatrix/EisenhowerMain.cs
@@ -37,15 +37,52 @@ namespace Codecool.EinsenhowerMatrix
                         case 0:
                             _cmd = false;
                             break;
+                        case 1:
+                            todoMatrix.ToString();
+                            break;
                         case 2:
-                            // TodoMatrix todoMatrix = new TodoMatrix();
-                            Console.WriteLine(todoMatrix.GetSpecificQuarter(TodoMatrix.QuarterTypes.UrgentAndImportant));
-                            Console.ReadKey();
+                            QuarterMenu(todoMatrix);
                             break;
                     }
                 }
             }
             while (_cmd);
+        }
+
+        private static void QuarterMenu(TodoMatrix todoMatrix)
+        {
+            Console.WriteLine("[ 1 ] Urgent and Important");
+            Console.WriteLine("[ 2 ] Not urgent and Important");
+            Console.WriteLine("[ 3 ] Urgent and Not important");
+            Console.WriteLine("[ 4 ] Not urgent and Not important");
+
+            int optionQuarter;
+            bool inputIsValidQuarter = int.TryParse(Console.ReadLine(), out optionQuarter);
+
+            if (inputIsValidQuarter)
+            {
+                switch (optionQuarter)
+                {
+                    case 1:
+                        Console.WriteLine(todoMatrix.GetSpecificQuarter(TodoMatrix.QuarterTypes.UrgentAndImportant));
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.WriteLine(todoMatrix.GetSpecificQuarter(TodoMatrix.QuarterTypes.NotUrgentAndImportant));
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine(todoMatrix.GetSpecificQuarter(TodoMatrix.QuarterTypes.UrgentAndNotimportant));
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine(todoMatrix.GetSpecificQuarter(TodoMatrix.QuarterTypes.NotUrgentAndNotimportant));
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
